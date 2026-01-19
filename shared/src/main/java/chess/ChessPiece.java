590bd8b06fcf27998ee933c323ffb9d32db41a34
Yuto Akatsuka <yuaka29@byu.edu>
2026-01-18 18:22:24 -0700

    Implemented the first position of Bishop

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -54,6 +54,10 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece= board.getPiece(myPosition);
+        if(piece.getPieceType() == PieceType.BISHOP){
+            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+
+        }
 
 
         return List.of();
