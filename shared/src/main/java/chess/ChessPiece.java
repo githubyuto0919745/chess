ec98ef2fe7b70dbff1391838985c4708976c0459
Yuto Akatsuka <yuaka29@byu.edu>
2026-01-22 15:07:47 -0700

    Completely implemented reset

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -641,5 +642,19 @@ private ArrayList<ChessMove> helperKnight(ChessBoard board, ChessPosition myPosi
         }
 
         return bishop;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
