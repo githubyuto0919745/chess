4de86b3558c1968ff97b381a1bba87c7579076c7
Yuto Akatsuka <yuaka29@byu.edu>
2026-02-23 20:32:04 -0700

    Working on handler in register

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,21 +1,37 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 public class Server {
-
+    ArrayList<String> users = new ArrayList<>();
     private final Javalin javalin;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        javalin.post("/user/{username}", this::handler);
 
     }
 
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
+    }
+    private void handler(Context ctx){
+        String username;
+        String password;
+        String email;
+
+        String register = ctx.pathParam("username");
+        users.add(register);
+    }
+    private void list(){
+
     }
 
     public void stop() {
