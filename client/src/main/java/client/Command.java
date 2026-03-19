6c293dc302ee5a299469f51313a722f091c77462
Yuto Akatsuka <yuaka29@byu.edu>
2026-03-19 16:16:55 -0600

    Updated command loop

--- client/src/main/java/client/Command.java
+++ client/src/main/java/client/Command.java
@@ -7,19 +7,65 @@ public class Command {
 
     }
 
-    public void command (){
-        System.out.print("Welcome to 240 chess. Type help to get started!!!");
-        Scanner options = new Scanner(System.in);
-        System.out.println("Enter your command >>>");
+    public void commands (){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Welcome to 240 chess. Type help to get started!!!");
+            System.out.println("[LOGGED_OUT]>>> ");
+            String command = scanner.next();
 
-        Scanner actions = new Scanner(System.in);
-        System.out.println("LOGGED_OUT >>>");
+            switch (command){
+                case "register":
+                    String username = scanner.next();
+                    String password = scanner.next();
+                    String email = scanner.next();
 
-        System.out.print("register <Username><Password><Email> - to create an account");
-        System.out.print("login <Username><Password> - to log in");
-        System.out.print("quit - playing chess");
-        System.out.print("help - with possible commands");
+                    System.out.print("Registered as " + username);
+                    break;
+                case "login":
+                    String user = scanner.next();
+                    String pass = scanner.next();
 
+                    System.out.print("Logged in as" + user);
+                    boolean loggedIn = true;
+
+                    while (loggedIn){
+                        System.out.print("create <NAME> - a game");
+                        System.out.print("list - games");
+                        System.out.print("join <ID> [WHITE|BLACK] - a game");
+                        System.out.print("observe <ID> - a game");
+                        System.out.print("logout - when you are done");
+                        System.out.print("quit - playing chess");
+                        System.out.print("help - with possible commands");
+
+                        String actions = scanner.next();
+                        System.out.println("LOGGED_IN >>>");
+                        switch (actions){
+                            case "create":
+                            case "list":
+                            case "join":
+                            case "observe":
+                            case "logout":
+                                loggedIn = false;
+                                break;
+                            case "quit":
+                            case "help":
+                        }
+                    }
+                case "quit":
+                    break;
+                case "help":
+                    System.out.print("register <Username><Password><Email>");
+                    System.out.print("login <Username><Password>");
+                    System.out.print("quit");
+                    System.out.print("help");
+                    break;
+            }
+
+
+
+
+        }
 
 
     }
