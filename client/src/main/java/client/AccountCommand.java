63d453174f5aaf038daeee3e48d3618b55ac3e71
Yuto Akatsuka <yuaka29@byu.edu>
2026-04-09 23:19:44 -0600

    Design fixed

--- client/src/main/java/client/AccountCommand.java
+++ client/src/main/java/client/AccountCommand.java
@@ -1,25 +1,20 @@
 package client;
-
 import chess.ChessGame;
 import record.AuthData;
 import record.GameData;
 import record.JoinGameRequest;
 import record.UserData;
 import ui.BoardDesign;
-
 import java.util.*;
-
 public class AccountCommand {
     private final HttpFacade httpFacade;
     private String authToken = null;
     private List<GameData> lastGame = new ArrayList<>();
     private BoardDesign board;
     private static String authUsername;
-
     public AccountCommand(String serverUrl){
         this.httpFacade = new HttpFacade(serverUrl);
     }
-
     public void commands (){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to 240 chess. Type help to get started!!!");
@@ -28,10 +23,8 @@ public class AccountCommand {
             String line = scanner.nextLine().trim();
             String[] parts = line.split("\\s+");
             String command = parts[0];
-
             switch (command.toLowerCase()) {
                 case "register" -> {
-
                     if (parts.length != 4){
                         System.out.println("You need to fill the information!");
                         break;
@@ -39,14 +32,12 @@ public class AccountCommand {
                     String username = parts[1];
                     String password =  parts[2];
                     String email =  parts[3];
-
                     try {
                         AuthData auth = httpFacade.register(new UserData(username, password, email));
                         authToken = auth.authToken();
                         authUsername = username.trim();
                         System.out.println("Registered as  " + username);
                         loggedinCommand(httpFacade, authToken, lastGame,board);
-
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
@@ -58,21 +49,16 @@ public class AccountCommand {
                     }
                     String user = parts[1];
                     String pass =  parts[2];
-
                     try {
                         AuthData auth = httpFacade.login(new UserData(user, pass, null));
                         authToken = auth.authToken();
                         authUsername = user.trim();
-
                         System.out.println("Logged in as  " + user);
                         System.out.println("Type help to get started!!!");
                         loggedinCommand(httpFacade, authToken, lastGame,board);
-
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
-
                     }
-
                 }case "quit" -> {
                     System.out.println("Thank you!");
                     return;
@@ -85,10 +71,7 @@ public class AccountCommand {
                 }
                 default ->{
                     System.out.println("[LOGGED_OUT]>>> ");
-                }
-            }
-        }
-    }
+                }}}}
     private static void loggedinCommand(HttpFacade httpFacade, String authToken, List<GameData> lastGame, BoardDesign board) throws Exception {
         boolean loggedIn = true;
         Scanner scanner = new Scanner(System.in);
@@ -100,32 +83,23 @@ public class AccountCommand {
             switch (actions.toLowerCase()) {
                 case "create" -> {
                     if (parts.length != 2){
-                        System.out.println("You need to fill the information!");
-                        break;}
+                        System.out.println("You need to fill the information!"); break;}
                     String name = parts[1];
                     try{
                         var game = httpFacade.createGame(new GameData(0,null,null,name,null), authToken);
                         lastGame.add(game);
                         System.out.println("Created game:  " + game.gameName());
                     }catch(ResponseException ex){
-                        System.out.println(ex.getMessage());
-                    }}
+                        System.out.println(ex.getMessage());}}
                 case "list" -> {
-                    try {
-                        lastGame = httpFacade.listGame(authToken);
+                    try {lastGame = httpFacade.listGame(authToken);
                         if(lastGame == null || lastGame.isEmpty()){
-                            System.out.println("No games available to join");
-                            break;
-                        }
+                            System.out.println("No games available to join");break;}
                         int displayIndex = 1;
                         for(GameData game: lastGame){
-                            System.out.println(displayIndex + ". " +
-                                    "GameName:"+game.gameName()
-                                    + "  WhitePlayer:" + game.whiteUsername() +
-                                    " BlackPlayer:" + game.blackUsername());
+                            System.out.println(displayIndex + ". " + "GameName:"+game.gameName() + "  WhitePlayer:" + game.whiteUsername() + " BlackPlayer:" + game.blackUsername());
                             displayIndex ++;}}catch(ResponseException ex){
-                        System.out.println(ex.getMessage());
-                    }}
+                        System.out.println(ex.getMessage());}}
                 case "join", "observe" -> {
                     Object[] array = helperChoiceColor(parts, lastGame);
                     if (array == null) break;
@@ -138,28 +112,19 @@ public class AccountCommand {
                     BoardDesign boardDesign = new BoardDesign(color);
                     boolean isObserver = !actions.equals("join");
                     PlayCommand playCommand = new PlayCommand(
-                            null,
-                            boardDesign,
-                            authToken,
-                            gameID,
-                            game,
-                            isObserver);
+                            null, boardDesign, authToken, gameID, game, isObserver);
                     WebSocketFacade wsFacade = new WebSocketFacade(playCommand);
                     playCommand.setWebSocketFacade(wsFacade);
                     if(actions.equals("join")){
-                        isObserver = false;
-                    }else{
-                        isObserver = true;
-                    }
+                        isObserver = false;}else{
+                        isObserver = true;}
                     if (actions.equals("join")) {
                         if ((user.equalsIgnoreCase(selectedGame.blackUsername())) || user.equalsIgnoreCase(selectedGame.whiteUsername())) {
                             System.out.println("You are already in the game! Rejoin the game!!");
-
                             board = new BoardDesign(color);
                             playCommand = new PlayCommand(wsFacade,board,authToken,gameID,game,false);
                             playCommand.wsCommand();
-                            return;
-                        }
+                            return;}
                         if ((selectedGame.whiteUsername() != null && !selectedGame.whiteUsername().isEmpty()) &&
                                 (selectedGame.blackUsername() != null && !selectedGame.blackUsername().isEmpty())) {
                             System.out.println("This game already has two players.");
@@ -175,7 +140,6 @@ public class AccountCommand {
                             System.out.println();
                             playCommand.wsCommand();
                             System.out.println();
-
                         } catch (ResponseException ex) {
                             System.out.println(ex.getMessage());}
                     } else {
@@ -186,10 +150,7 @@ public class AccountCommand {
                         board = new BoardDesign(color);
                         System.out.println();
                         playCommand.wsCommand();
-                        System.out.println();
-
-                    }
-                }
+                        System.out.println();}}
                 case "logout" -> {
                     try {
                         httpFacade.logout(authToken);
@@ -201,8 +162,7 @@ public class AccountCommand {
                     }catch(ResponseException ex){
                         System.out.println(ex.getMessage());}}
                 case "quit" -> {
-                    System.out.println("Thank you for playing!");
-                    return;}
+                    System.out.println("Thank you for playing!");return;}
                 case "help" -> {
                     System.out.println("create <NAME>");
                     System.out.println("list");
@@ -212,7 +172,6 @@ public class AccountCommand {
                     System.out.println("quit");
                     System.out.println("help");
                 }}}}
-
     private static Object[] helperChoiceColor(String[] parts, List<GameData> lastGame){
         if (parts.length != 3){
             System.out.println("You need to fill the information!");
@@ -234,8 +193,4 @@ public class AccountCommand {
             return null;
         }
         return new Object[]{choice -1, color};
-
-    }
-
-
-}
+    }}
--- client/src/main/java/client/PlayCommand.java
+++ client/src/main/java/client/PlayCommand.java
@@ -1,60 +1,41 @@
 package client;
-
 import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import ui.BoardDesign;
-
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-
 public class PlayCommand {
     private WebSocketFacade wsFacade;
     private final String authToken;
     private Integer gameID;
     private final BoardDesign boardDesign;
     private ChessGame game;
-    private final boolean isObserver;
-
-
-    public PlayCommand(WebSocketFacade wsFacade, BoardDesign boardDesign, String authToken, Integer gameID, ChessGame game, boolean isObserver) {
+    private final boolean isObserver;public PlayCommand(WebSocketFacade wsFacade, BoardDesign boardDesign, String authToken, Integer gameID, ChessGame game, boolean isObserver) {
         this.wsFacade = wsFacade;
         this.boardDesign = boardDesign;
         this.authToken = authToken;
         this.gameID= gameID;
         this.game = game;
-        this.isObserver = isObserver;
-    }
+        this.isObserver = isObserver;}
     public void updateGame(ChessGame newGame){
         this.game = newGame;
         boardDesign.updateGame(newGame);
-        boardDesign.printBoard(null);
-    }
+        boardDesign.printBoard(null);}
     public void setWebSocketFacade(WebSocketFacade wsFacade) {
         this.wsFacade = wsFacade;
     }
     private int convertColumn(String col){
     return switch (col.toLowerCase()) {
-        case "a" -> 1;
-        case "b" -> 2;
-        case "c" -> 3;
-        case "d" -> 4;
-        case "e" -> 5;
-        case "f" -> 6;
-        case "g" -> 7;
-        case "h" -> 8;
-        default -> throw new IllegalArgumentException("Invalid column: " + col);
-    };
-    }
+        case "a" -> 1; case "b" -> 2; case "c" -> 3; case "d" -> 4; case "e" -> 5; case "f" -> 6; case "g" -> 7;case "h" -> 8;
+        default -> throw new IllegalArgumentException("Invalid column: " + col);};}
     public void wsCommand() {
         Scanner scanner = new Scanner(System.in);
         wsFacade.connect(authToken,gameID);
-
         boolean isJoined = true;
-
         while (isJoined) {
             System.out.println("JOINED_IN >>>");
             String line = scanner.nextLine().trim();
@@ -65,161 +46,121 @@ public class PlayCommand {
                 case "help" -> {
                     if (parts.length != 1){
                         System.out.println("You need to fill the information!");
-                        break;
-                    }
+                        break;}
                     System.out.println("redraw");
                     System.out.println("leave");
                     System.out.println("resign");
                     System.out.println("move");
-                    System.out.println("highlight");
-                }
+                    System.out.println("highlight");}
                 case "redraw" -> {
                     if (parts.length != 1){
                         System.out.println("You need to fill the information!");
-                        break;
-                    }
+                        break;}
                     boardDesign.updateGame(game);
-                    boardDesign.printBoard(null);
-                }
+                    boardDesign.printBoard(null);}
                 case "leave" -> {
                     if (parts.length != 1){
                         System.out.println("You need to fill the information!");
-                        break;
-                    }
+                        break;}
                     wsFacade.leave(authToken, gameID);
                     isJoined = false;
                     gameID = null;
-                    continue;
-                }
+                    continue;}
                 case "move" -> {
                     if (parts.length != 1){
                         System.out.println("You need to fill the information!");
                         break;
                     }
-                    if(isObserver){
-                        System.out.println("Observers cannot make moves ");
-                        break;
-                    }
-                    System.out.print("From row:  (1-8) ");
-                    String fromRowInput = scanner.nextLine();
-                    int fr;
-                    try{
-                        fr =  Integer.parseInt(fromRowInput);
-                    }catch (NumberFormatException e){
-                        System.out.println("Invalid row: must be a number(1-8)");
-                        break;
-                    }
-                    if(fr < 1 || fr > 8){
-                        System.out.println("Row must be between 1 - 8");
-                        break;
-                    }
-
-                    System.out.print("From col:  (a-h) ");
-                    String fromColInput = scanner.nextLine().trim().toLowerCase();
-                    if(fromColInput.length() !=1 || fromColInput.charAt(0) < 'a' || fromColInput.charAt(0) > 'h'){
-                        System.out.println("Invalid column: must be a letter (a-h)");
-                        break;
-                    }
-
-                    int fc = fromColInput.charAt(0) - 'a' + 1;
-
-
-                    System.out.print("To row:  (1-8) ");
-                    int tr = Integer.parseInt(scanner.nextLine());
-                    if(tr < 1 || tr > 8){
-                        System.out.println("Invalid input: Row must be between (1-8)");
-                        break;
-                    }
-                    System.out.print("To col:  (a-h) ");
-                    String toColInput = scanner.nextLine().trim().toLowerCase();
-                    if(toColInput.length() !=1 || toColInput.charAt(0) < 'a' || toColInput.charAt(0) > 'h'){
-                        System.out.println("Invalid column: must be a letter (a-h)");
-                        break;
-                    }
-                    int tc = toColInput.charAt(0) - 'a' + 1;
-
-                    ChessPosition from = new ChessPosition(fr,fc);
-                    ChessPosition to = new ChessPosition(tr,tc);
-
-                    ChessPiece piece = game.getBoard().getPiece(from);
-                    ChessPiece.PieceType promotion = null;
-
-                    if(piece != null &&
-                        piece.getPieceType() == ChessPiece.PieceType.PAWN){
-                        boolean whitePromote = piece.getTeamColor() == ChessGame.TeamColor.WHITE && tr == 8;
-                        boolean blackPromote = piece.getTeamColor() == ChessGame.TeamColor.BLACK && tr == 8;
-
-                        if(whitePromote || blackPromote){
-                            PawnPromotion pro = new PawnPromotion();
-                            promotion = pro.askPromotion();
-                        }
-                    }
-                    ChessMove move = new ChessMove(from, to, promotion);
-                    wsFacade.move(authToken, gameID, move);
-                }
-                case "resign" -> {
-                    if (parts.length != 1){
-                        System.out.println("You need to fill the information!");
-                        break;
-                    }
-                    if(isObserver){
-                        System.out.println("Observers cannot resign ");
-                        break;
-                    }
-                    System.out.print("Are you sure to resign? (yes/no) ");
-                    String response = scanner.nextLine().trim().toLowerCase();
-                    if(response.equals("yes")){
-                        wsFacade.resign(authToken, gameID);
-                    }else{
-                        continue;
-                    }
-
+                    handleMove(scanner);
                 }
                 case "highlight" ->{
                     if (parts.length != 1){
                         System.out.println("You need to fill the information!");
-                        break;
-                    }
+                        break;}
                     System.out.print("Row (1-8):  ");
                     String rowInput = scanner.nextLine().trim();
-
                     System.out.print("Col (a-h):  ");
                     String colInput = scanner.nextLine().trim().toLowerCase();
-
                     int row;
-                    try{
-                        row =  Integer.parseInt(rowInput);
+                    try{row =  Integer.parseInt(rowInput);
                     }catch (NumberFormatException e){
                         System.out.println("Invalid row: must be a number(1-8)");
-                        break;
-                    }
+                        break;}
                     if(row <1 || row > 8){
                         System.out.println("Row must be 1-8");
-                        break;
-                    }
+                        break;}
                     if(colInput.length() != 1|| colInput.charAt(0) < 'a' || colInput.charAt(0) > 'h'){
                         System.out.println("Invalid column: must be a letter (a-h)");
-                        break;
-                    }
+                        break;}
                     int col = convertColumn(colInput);
-
                     ChessPosition pos = new ChessPosition(row,col);
                     ChessPiece piece = game.getBoard().getPiece(pos);
                     if(piece == null){
                         System.out.println("Empty square selected");
-                        break;
-                    }
+                        break;}
                     Collection<ChessMove> moved = game.validMoves(pos);
                     Set<ChessPosition> highlights = new HashSet<>();
                     for(ChessMove move: moved){
-                        highlights.add(move.getEndPosition());
-                    }
+                        highlights.add(move.getEndPosition());}
                     boardDesign.updateGame(game);
                     boardDesign.printBoard(highlights);
-
                 }
-
             }
         }
     }
+    private void handleMove(Scanner scanner){
+
+        if(isObserver){
+            System.out.println("Observers cannot make moves ");
+            return;}
+        System.out.print("From row:  (1-8) ");
+        String fromRowInput = scanner.nextLine();
+        int fr;
+        try{
+            fr =  Integer.parseInt(fromRowInput);
+        }catch (NumberFormatException e){
+            System.out.println("Invalid row: must be a number(1-8)");
+            return;
+        }
+        if(fr < 1 || fr > 8){
+            System.out.println("Row must be between 1 - 8");
+            return;
+        }
+        System.out.print("From col:  (a-h) ");
+        String fromColInput = scanner.nextLine().trim().toLowerCase();
+        if(fromColInput.length() !=1 || fromColInput.charAt(0) < 'a' || fromColInput.charAt(0) > 'h'){
+            System.out.println("Invalid column: must be a letter (a-h)");
+            return;
+        }
+        int fc = fromColInput.charAt(0) - 'a' + 1;
+        System.out.print("To row:  (1-8) ");
+        int tr = Integer.parseInt(scanner.nextLine());
+        if(tr < 1 || tr > 8){
+            System.out.println("Invalid input: Row must be between (1-8)");
+            return;
+        }
+        System.out.print("To col:  (a-h) ");
+        String toColInput = scanner.nextLine().trim().toLowerCase();
+        if(toColInput.length() !=1 || toColInput.charAt(0) < 'a' || toColInput.charAt(0) > 'h'){
+            System.out.println("Invalid column: must be a letter (a-h)");
+            return;
+        }
+        int tc = toColInput.charAt(0) - 'a' + 1;
+        ChessPosition from = new ChessPosition(fr,fc);
+        ChessPosition to = new ChessPosition(tr,tc);
+        ChessPiece piece = game.getBoard().getPiece(from);
+        ChessPiece.PieceType promotion = null;
+        if(piece != null &&
+                piece.getPieceType() == ChessPiece.PieceType.PAWN){
+            boolean whitePromote = piece.getTeamColor() == ChessGame.TeamColor.WHITE && tr == 8;
+            boolean blackPromote = piece.getTeamColor() == ChessGame.TeamColor.BLACK && tr == 8;
+            if(whitePromote || blackPromote){
+                PawnPromotion pro = new PawnPromotion();
+                promotion = pro.askPromotion();}}
+        ChessMove move = new ChessMove(from, to, promotion);
+        wsFacade.move(authToken, gameID, move);
+    }
+
+
+
 }
